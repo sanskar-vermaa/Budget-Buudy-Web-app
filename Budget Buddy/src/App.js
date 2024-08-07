8498798933cbaf9d15e0d6d737b6f82e2f067d4b
Sanskar Verma <sanskarv120503@gmail.com>
2024-08-07 21:52:00 +0530

    Initial commit

---
+++ Budget Buddy/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import SignUpSignIn from "./components/Signup";
+
+function App() {
+  return (
+    <div>
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUpSignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+      </Routes>
+    </Router>
+  
+      
+    </div>
+  );
+}
+
+export default App;
